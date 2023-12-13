45d13d1cb307918d7f07db7dd403312420f87c5c
danielemiche <74101475+danielemiche@users.noreply.github.com>
2023-12-12 23:48:57 -0800

    adding plots

--- Experiments.py
+++ Experiments.py
@@ -104,6 +104,90 @@ plt.tight_layout()
 plt.savefig('hist_log.png')
 plt.show()
 
+# Plot the traceplots of the estimates for the beta
+fig, ax = plt.subplots(3, 3, figsize=(15, 5))
+for i in range(3):
+    ax[i, 0].plot(betas_HMC[:, i])
+    ax[i, 1].plot(betas_MH[:, i])
+    ax[i, 2].plot(betas_Gibbs[:, i])
+    ax[i, 0].axhline(y=betas[i], color='r', linestyle='-', label='True')
+    ax[i, 1].axhline(y=betas[i], color='r', linestyle='-', label='True')
+    ax[i, 2].axhline(y=betas[i], color='r', linestyle='-', label='True')
+    ax[i, 0].axhline(y=MLE[i], color='g', linestyle='--', label='MLE', alpha=0.5)
+    ax[i, 1].axhline(y=MLE[i], color='g', linestyle='--', label='MLE', alpha=0.5)
+    ax[i, 2].axhline(y=MLE[i], color='g', linestyle='--', label='MLE', alpha=0.5)
+    ax[i, 0].set_ylabel('Beta {}'.format(i+1))
+    ax[i, 1].set_yticklabels([])
+    ax[i, 2].set_yticklabels([])
+ax[0, 0].set_title('HMC')
+ax[0, 1].set_title('RH')
+ax[0, 2].set_title('RHWG')
+ax[0, 0].legend()
+plt.tight_layout()
+plt.savefig('trace_log.png')
+plt.show()
+
+# Plot the autocorrelation of the estimates for the beta
+fig, ax = plt.subplots(3, 3, figsize=(15, 5))
+for i in range(3):
+    ax[i, 0].acorr(betas_HMC[:, i] - np.mean(betas_HMC[:, i]), maxlags=100)
+    ax[i, 1].acorr(betas_MH[:, i] - np.mean(betas_MH[:, i]), maxlags=100)
+    ax[i, 2].acorr(betas_Gibbs[:, i] - np.mean(betas_Gibbs[:, i]), maxlags=100)
+    ax[i, 0].set_ylabel('Beta {}'.format(i+1))
+    ax[i, 1].set_yticklabels([])
+    ax[i, 2].set_yticklabels([])
+ax[0, 0].set_title('HMC')
+ax[0, 1].set_title('RH')
+ax[0, 2].set_title('RHWG')
+plt.tight_layout()
+plt.savefig('autocorr_log.png')
+plt.show()
+
+# Experiment 2: ring shape distribution
+
+def ring_shaped_distribution(x, y):
+    return np.exp(-0.2 * (x**2 + y**2) - 0.1 * (x**2 - y**2))
+
+def log_ring_shaped_distribution(x, y):
+    return -0.2 * (x**2 + y**2) - 0.1 * (x**2 - y**2)
+
+def grad_log_ring_shaped_distribution(x, y):
+    return np.array([-0.4 * x - 0.2 * x, -0.4 * y + 0.2 * y])
+
+logp = lambda theta: log_ring_shaped_distribution(theta[0], theta[1])
+U = lambda theta: -logp(theta)
+gradU = lambda theta: -grad_log_ring_shaped_distribution(theta[0], theta[1])
+
+T = 10000
+burnin = 1000
+
+theta0 = np.array([0, 0])
+
+thetas_MH = Metropolis_Hastings(logp, theta0, scale=1, T=T, burnin=burnin, thin=1)
+thetas_Gibbs = Gibbs_within_MH_rand(logp, theta0, scale=1, T=T, burnin=burnin, thin=1)
+thetas_HMC = HMC(U, gradU, theta0, L=10, eps=0.1, scale=1, T=T, burnin=burnin, thin=1)
+
+# Plot the trajectory of the Markov Chain for the three algorithms in a contour plot of the distribution
+x = np.linspace(-3, 3, 100)
+y = np.linspace(-3, 3, 100)
+X, Y = np.meshgrid(x, y)
+Z = ring_shaped_distribution(X, Y)
+
+fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+ax[0].contour(X, Y, Z)
+ax[0].plot(thetas_HMC[:, 0], thetas_HMC[:, 1], color='r')
+ax[0].set_title('HMC')
+ax[1].contour(X, Y, Z)
+ax[1].plot(thetas_MH[:, 0], thetas_MH[:, 1], color='r')
+ax[1].set_title('RH')
+ax[2].contour(X, Y, Z)
+ax[2].plot(thetas_Gibbs[:, 0], thetas_Gibbs[:, 1], color='r')
+ax[2].set_title('RHWG')
+plt.tight_layout()
+plt.savefig('contour.png')
+plt.show()
+
+
 
 
 
